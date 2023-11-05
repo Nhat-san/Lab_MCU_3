979e734673011f924d9288233da393cafc205ce0
Nhat-san <huahoangnhat01113@gmail.com>
2023-11-05 10:07:36 +0700

    test button

---
+++ Src/Core/Inc/Button_reading.h
@@ -0,0 +1,12 @@
+
+#ifndef INC_INPUT_PROCESSING_H_
+#define INC_INPUT_PROCESSING_H_
+#include <stdint.h>
+#include <stdbool.h>
+#include "main.h"
+
+void initButton();
+uint8_t is_button_pressed(uint8_t i);
+uint8_t is_button_long_pressed(uint8_t i);
+
+#endif /* INC_INPUT_PROCESSING_H_ */
